afdfc580426498e987d9a1d20804add3f78b980d
abdulqziz2009 <148250194+abdulqziz2009@users.noreply.github.com>
2023-11-04 13:15:00 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+            public static void main(String[] args) {
+                double[] numbers = {8, -2, -4, 2, 3, 6, -7};
+
+                double sum = 0;
+                boolean foundNegative = false;
+                int count = 0;
+
+                for (double num : numbers) {
+                    if (foundNegative && num > 0) {
+                        sum += num;
+                        count++;
+                    } else if (num < 0) {
+                        foundNegative = true;
+                    }
+                }
+
+                double average = sum / count;
+
+
+                System.out.println("Среднее арифметическое положительных чисел после первого отрицательного: " + average);
+            }
+        }
